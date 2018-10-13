3b38c79f960fcadfe4cdcb31954461507e60d852
sebmichalowski <seb.michalowski@gmail.com>
2018-10-13 17:21:16 +0200

    Model Refactor

--- src/main/java/pl/websm/volunteers/model/Event.java
+++ src/main/java/pl/websm/volunteers/model/Event.java
@@ -1,6 +1,7 @@
 package pl.websm.volunteers.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.geo.Point;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.time.LocalDateTime;
@@ -11,4 +12,13 @@ public class Event {
     private String id;
     private LocalDateTime localDateTime;
     private Position position;
+
+    public Event(){
+
+    }
+
+    public Event(LocalDateTime localDateTime, Position position) {
+        this.localDateTime = localDateTime;
+        this.position = position;
+    }
 }
--- src/main/java/pl/websm/volunteers/model/Position.java
+++ src/main/java/pl/websm/volunteers/model/Position.java
@@ -1,11 +1,19 @@
 package pl.websm.volunteers.model;
 
 import lombok.Data;
+import org.springframework.data.geo.Point;
 import org.springframework.data.redis.core.index.GeoIndexed;
+
 
 @Data
 public class Position {
     @GeoIndexed
-    private Double latitude;
-    private Double longitude;
+    private Point point;
+
+    public Position(){
+
+    }
+    public Position(Double latitude, Double longitude) {
+        point = new Point(latitude, longitude);
+    }
 }
