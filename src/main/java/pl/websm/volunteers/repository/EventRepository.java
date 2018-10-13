edbc08ff47b81b7bbdff0a2a1d79abc313421f55
sebmichalowski <seb.michalowski@gmail.com>
2018-10-13 17:35:22 +0200

    Add eventRepository findByNear method

---
+++ src/main/java/pl/websm/volunteers/repository/EventRepository.java
@@ -0,0 +1,14 @@
+package pl.websm.volunteers.repository;
+
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Point;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.websm.volunteers.model.Event;
+
+import java.util.Set;
+
+@Repository
+public interface EventRepository extends CrudRepository<Event, String> {
+    Set<Event> findByPositionPointNear(Point point, Distance distance);
+}
