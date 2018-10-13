1e10f1063c7565733430b32ee8dc682155853ef6
sebmichalowski <seb.michalowski@gmail.com>
2018-10-13 17:20:16 +0200

    Add Redis Template bean in redisConfig refactor

--- src/main/java/pl/websm/volunteers/configuration/redisConfig.java
+++ src/main/java/pl/websm/volunteers/configuration/redisConfig.java
@@ -4,13 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import pl.websm.volunteers.model.Organization;
 
 @Configuration
 public class redisConfig {
-        @Bean
-        public LettuceConnectionFactory redisConnectionFactory() {
 
-            return new LettuceConnectionFactory(new RedisStandaloneConfiguration("server", 6379));
-        }
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration());
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
 }
 
