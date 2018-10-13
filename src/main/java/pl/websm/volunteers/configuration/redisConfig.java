fe01c257f1ac69235b75dd999b4f21210a77264f
sebmichalowski <seb.michalowski@gmail.com>
2018-10-13 13:57:34 +0200

    Add redis lettuceConnection

---
+++ src/main/java/pl/websm/volunteers/configuration/redisConfig.java
@@ -0,0 +1,16 @@
+package pl.websm.volunteers.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class redisConfig {
+        @Bean
+        public LettuceConnectionFactory redisConnectionFactory() {
+
+            return new LettuceConnectionFactory(new RedisStandaloneConfiguration("server", 6379));
+        }
+}
+
