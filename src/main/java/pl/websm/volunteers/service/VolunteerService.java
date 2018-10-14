a230b82aadfc202933abe61b4e20e0e58c9ae5de
sebmichalowski <seb.michalowski@gmail.com>
2018-10-14 02:06:20 +0200

    Add VolunteerService

--- src/main/java/pl/websm/volunteers/service/VolunteerService.java
+++ src/main/java/pl/websm/volunteers/service/VolunteerService.java
@@ -5,8 +5,10 @@ import org.springframework.data.geo.Point;
 import pl.websm.volunteers.model.Event;
 import pl.websm.volunteers.model.Volunteer;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface VolunteerService {
     Set<Event> findNearbyEvents(Point point, Distance distance);
+    Optional<Volunteer> getVolunteer(String id);
 }
--- src/main/java/pl/websm/volunteers/service/VolunteerServiceImpl.java
+++ src/main/java/pl/websm/volunteers/service/VolunteerServiceImpl.java
@@ -3,19 +3,28 @@ package pl.websm.volunteers.service;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Point;
 import pl.websm.volunteers.model.Event;
+import pl.websm.volunteers.model.Volunteer;
 import pl.websm.volunteers.repository.EventRepository;
+import pl.websm.volunteers.repository.VolunteerRepository;
 
+import java.util.Optional;
 import java.util.Set;
 
 public class VolunteerServiceImpl implements VolunteerService {
     private final EventRepository eventRepository;
+    private final VolunteerRepository volunteerRepository;
 
-    public VolunteerServiceImpl(EventRepository eventRepository) {
+    public VolunteerServiceImpl(EventRepository eventRepository, VolunteerRepository volunteerRepository) {
         this.eventRepository = eventRepository;
+        this.volunteerRepository = volunteerRepository;
     }
 
     @Override
     public Set<Event> findNearbyEvents(Point point, Distance distance) {
         return eventRepository.findByPositionPointNear(point, distance);
+    }
+
+    public Optional<Volunteer> getVolunteer(String id) {
+        return volunteerRepository.findById(id);
     }
 }
