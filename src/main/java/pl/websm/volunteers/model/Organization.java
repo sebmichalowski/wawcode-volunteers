05d5c4adf078f33b4a72683b269548b0ac7b51fd
sebmichalowski <seb.michalowski@gmail.com>
2018-10-14 02:32:55 +0200

    Update on organization model, Add method in OrganizationService

--- src/main/java/pl/websm/volunteers/model/Organization.java
+++ src/main/java/pl/websm/volunteers/model/Organization.java
@@ -1,8 +1,9 @@
 package pl.websm.volunteers.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
-
+@Data
 @RedisHash("organization")
 public class Organization {
     @Id
--- src/main/java/pl/websm/volunteers/service/OrganizationService.java
+++ src/main/java/pl/websm/volunteers/service/OrganizationService.java
@@ -2,10 +2,13 @@ package pl.websm.volunteers.service;
 
 import org.springframework.data.geo.Point;
 import pl.websm.volunteers.model.AreaOfExpertise;
+import pl.websm.volunteers.model.Organization;
 import pl.websm.volunteers.model.Volunteer;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface OrganizationService {
     Set<Volunteer> findVolunteerByAreaOfExpertise(Point point, AreaOfExpertise areaOFExpertise);
+    Optional<Organization> getOrganization(String id);
 }
